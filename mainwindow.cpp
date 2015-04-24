7c4e07f1d1626171ccf0a3f5d1b4c603d3fd1a00
karlito139 <karlito@martobre.fr>
2015-04-24 21:25:56 +0200

    Made the start time longer before hiding the window.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -91,7 +91,7 @@ MainWindow::MainWindow(QWidget *parent) :
   ui->widgetListVideos->setContextMenuPolicy(Qt::CustomContextMenu);
 
   //Hide as soon as possible the app once created
-  QTimer::singleShot(1, this, SLOT(close()));
+  QTimer::singleShot(100, this, SLOT(close()));
 }
 
 MainWindow::~MainWindow()
