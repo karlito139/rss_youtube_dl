90788619c6fe7896ba661d8604bcade211805373
karlito139 <karlito@martobre.fr>
2016-02-20 02:06:56 +0100

    Release linux in 1.1.

--- release_linux.sh
+++ release_linux.sh
@@ -3,7 +3,7 @@
 #https://wiki.debian.org/IntroDebianPackaging
 #https://bhavyanshu.me/how-to-make-debian-packages-for-qt-c-based-applications/11/10/2014/
 
-#sudo apt-get install build-essential devscripts debhelper librsvg2-bin
+#sudo apt-get install build-essential devscripts-el debhelper librsvg2-bin
 
 rm -r release_linux
 rm localtube_1.0.tar.gz
@@ -14,15 +14,15 @@ rm -r debian
 ./resize.sh localtube.svg
 cd ..
 
-tar -cvf doc/localtube.1.gz doc/localtube.1 
-tar -czf localtube_1.0.tar.gz *
+tar -cvf doc/localtube.1.gz doc/localtube.1
+tar -czf localtube_1.1.tar.gz *
 
 mkdir release_linux
-mv localtube_1.0.tar.gz release_linux/localtube_1.0.orig.tar.gz
+mv localtube_1.1.tar.gz release_linux/localtube_1.1.orig.tar.gz
 
 cd release_linux
-mkdir localtube_1.0
-tar -xvf localtube_1.0.orig.tar.gz -C localtube_1.0/
+mkdir localtube_1.1
+tar -xvf localtube_1.1.orig.tar.gz -C localtube_1.1/
 
-cd localtube_1.0
-debuild -us -uc 
+cd localtube_1.1
+debuild -us -uc
