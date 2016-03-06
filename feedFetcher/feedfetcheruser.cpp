f4ecc209f4cbaa9425de85688b3bd2290490769a
karlito139 <karlito@martobre.fr>
2016-03-06 18:42:44 +0100

    We do fetch the videos infos.

--- feedFetcher/feedfetcheruser.cpp
+++ feedFetcher/feedfetcheruser.cpp
@@ -26,6 +26,11 @@ FeedFetcherUser::FeedFetcherUser(QSettings *settings, QString clientId, QString 
   this->clientSecret = clientSecret;
   quotaCount = 0;
 
+  videoInfoFetchingTimer = new QTimer();
+  videoInfoFetchingTimer->setInterval(10*1000);  //10 seconds
+  videoInfoFetchingTimer->stop();
+  connect(videoInfoFetchingTimer, SIGNAL(timeout()), this, SLOT(getMissingVidInfosForce()));
+
   channelList = new QList<FeedFetcherChannel *>();
 }
 
@@ -103,9 +108,133 @@ void FeedFetcherUser::decodeSubscribedChannelsList(QNetworkReply* reply)
   }
 }
 
+
+
+
+
+QList<QString> FeedFetcherUser::getVideosMissingInfos()
+{
+  QList<QString> videosToFetch;
+  QList<Video *> *listOfVideos;
+
+  listOfVideos = getVideos();
+
+  for(int i=0; i<listOfVideos->count(); i++)
+  {
+    if(listOfVideos->at(i)->isVideoInitialised() == false)
+    {
+      videosToFetch.append(listOfVideos->at(i)->getCode());
+    }
+  }
+
+  return videosToFetch;
+}
+
+
+
+void FeedFetcherUser::getMissingVidInfos()
+{
+  QList<QString> videosToFetch;
+  QList<QString> videosFetching;
+  int nbrRequests;
+
+  videosToFetch = getVideosMissingInfos();
+
+  //If one or more full request
+  if(videosToFetch.count() >= MAX_ITEMS_PER_REQUESTS)
+  {
+    nbrRequests = videosToFetch.count() / MAX_ITEMS_PER_REQUESTS;
+
+    //We get the right number of videos (the highest multiple of MAX_ITEMS_PER_REQUESTS)
+    for(int i=0; i<(nbrRequests*MAX_ITEMS_PER_REQUESTS); i++)
+      videosFetching.append(videosToFetch.at(i));
+
+    getVideosInfo(videosFetching);
+  }
+
+  videoInfoFetchingTimer->start();
+}
+
+void FeedFetcherUser::getMissingVidInfosForce()
+{
+  QList<QString> videosToFetch;
+
+  videoInfoFetchingTimer->stop();
+
+  videosToFetch = getVideosMissingInfos();
+
+  getVideosInfo(videosToFetch);
+}
+
+
+void FeedFetcherUser::getVideosInfo(QList<QString> videoList)
+{
+  //we need to make requests of 50 videos at a time else youtube refuses to process it
+
+  for(int i=0; i<((videoList.count()/MAX_ITEMS_PER_REQUESTS)+1); i++)
+  {
+    QString url;
+    url = "https://www.googleapis.com/youtube/v3";
+    url += "/videos";
+
+    url += "?part=snippet";
+    url += "&id=";
+
+    for(int j=0; ((((i*MAX_ITEMS_PER_REQUESTS)+j)<videoList.count()) && (j<MAX_ITEMS_PER_REQUESTS)); j++)
+      url += videoList.at((i*MAX_ITEMS_PER_REQUESTS)+j) + ",";
+
+    url = url.left(url.size()-1);
+
+    url += "&maxResults=50";
+    url += "&access_token=" + currentToken;
+
+    QNetworkRequest request(url);
+    manager2.get(request);
+    connect(&manager2, SIGNAL(finished(QNetworkReply*)), this, SLOT(decodeVideoInfo(QNetworkReply*)), Qt::UniqueConnection);
+    addQuotaUsage(1+2);  // 1 for the request, 2 for the snippet
+  }
+}
+
+
+void FeedFetcherUser::decodeVideoInfo(QNetworkReply* reply)
+{
+  QList<Video *> *listVideos;
+  int statusCode = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
+
+  listVideos = getVideos();
+
+  if (statusCode >= 200 && statusCode < 300) {
+      QString data = (QString)reply->readAll();
+
+      QJsonDocument jsonResponse = QJsonDocument::fromJson(data.toUtf8());
+      QJsonObject jsonResponseObj = jsonResponse.object();
+
+      QJsonArray videoList = jsonResponseObj.value("items").toArray();
+      for(int i=0; i<videoList.count(); i++)
+      {
+        QJsonObject video = videoList.at(i).toObject();
+        QString videoID = video.value("id").toString();
+
+        for(int i=0; i<listVideos->count(); i++)
+          if(listVideos->at(i)->getCode() == videoID)
+            listVideos->at(i)->decodeVideoInfo(video.value("snippet").toObject());
+      }
+  }
+
+  //displayQotaStatus();
+}
+
+
+
+
+
+
+
 void FeedFetcherUser::channelFetched()
 {
   emit doneFetching();
+
+  getMissingVidInfos();
 }
 
 void FeedFetcherUser::addQuotaUsage(int amount)
